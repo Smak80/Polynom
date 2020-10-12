1044f5d8ef5b4bb56e0449a36a8a2a51b8815644
Smak80 <smaklets@kpfu.ru>
2020-10-12 11:03:21 +0300

    Класс конвертации координат со вспомогательными классами. + Изменение методов на статичные

--- src/ru/smak/gui/graphics/coordinatesystem/Converter.java
+++ src/ru/smak/gui/graphics/coordinatesystem/Converter.java
@@ -11,7 +11,7 @@ class Converter {
      * @param plane плоскость, для которой осуществляется преобразование
      * @return абсцисса точки в экранной системе координат
      */
-    int xCrt2Scr(double x, CartesianScreenPlane plane) {
+    static int xCrt2Scr(double x, CartesianScreenPlane plane) {
         var r = (int)(plane.getXDen() * (x - plane.getXMin()));
         if (r < -plane.getWidth()) r = -plane.getWidth();
         if (r > 2 * plane.getWidth()) r = 2 * plane.getWidth();
@@ -25,7 +25,7 @@ class Converter {
      * @param plane плоскость, для которой осуществляется преобразование
      * @return абсцисса точки в декартовой системе координат
      */
-    double xScr2Crt(int x, CartesianScreenPlane plane) {
+    static double xScr2Crt(int x, CartesianScreenPlane plane) {
         if (x < -plane.getWidth()) x = -plane.getWidth();
         if (x > 2 * plane.getWidth()) x = 2 * plane.getWidth();
         return x / plane.getXDen() + plane.getXMin();
@@ -38,7 +38,7 @@ class Converter {
      * @param plane плоскость, для которой осуществляется преобразование
      * @return ордината точки в экранной системе координат
      */
-    int yCrt2Scr(double y, CartesianScreenPlane plane) {
+    static int yCrt2Scr(double y, CartesianScreenPlane plane) {
         var r = (int)(plane.getYDen() * (plane.getYMax() - y));
         if (r < -plane.getHeight()) r = -plane.getHeight();
         if (r > 2 * plane.getHeight() + 10) r = 2 * plane.getHeight();
@@ -52,7 +52,7 @@ class Converter {
      * @param plane плоскость, для которой осуществляется преобразование
      * @return ордината точки в декартовой системе координат
      */
-    double yScr2Crt(int y, CartesianScreenPlane plane){
+    static double yScr2Crt(int y, CartesianScreenPlane plane){
         if (y < -plane.getHeight()) y = -plane.getHeight();
         if (y > 2 * plane.getHeight()) y = 2 * plane.getHeight();
         return plane.getYMax() - y / plane.getYDen();
